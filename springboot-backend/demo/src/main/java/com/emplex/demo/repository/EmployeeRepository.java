4f8fafa6d6732be8d4a8e80e3f1402af2919981c
Cosmin <52450104+cosminnitu@users.noreply.github.com>
2021-04-15 00:30:02 +0300

    first commit

---
+++ springboot-backend/demo/src/main/java/com/emplex/demo/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.emplex.demo.repository;
+
+import com.emplex.demo.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
